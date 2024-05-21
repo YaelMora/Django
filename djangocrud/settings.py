47f6a63dc726a292174abbcda3b198887bd351b8
Yael Lugo <ylugo@nanuk.mx>
2024-05-21 11:23:19 -0600

    agregar static files

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    python manage.py collectstatic --no-input# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    pyth# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
